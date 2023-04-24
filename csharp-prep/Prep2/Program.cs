97acd302e68f5aeb08a8570d7328d0c9e1d00701
omoralesb <oscarmb_6@hotmail.com>
2023-04-23 23:28:41 -0500

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class this time. Don't give up. Try again next time.");
+        }
     }
 }
