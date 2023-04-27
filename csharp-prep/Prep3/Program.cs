bf59f97e889c1ebe4185ceffd30ea58a42144840
omoralesb <oscarmb_6@hotmail.com>
2023-04-27 00:11:43 -0500

    W02 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = Convert.ToInt32(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int userGuess = -1;
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Try lower");
+            }    
+            if (userGuess < magicNumber)
+            {    
+                Console.WriteLine("Try higher");
+            }
+            else 
+            {
+                Console.Write("Congrats, you guessed it!");
+            }       
+        }                  
     }
 }
